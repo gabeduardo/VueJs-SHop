5e657282bac3d8a476bc8cce4bbf3dfb44230475
Gabo <gemcirelli1@gmail.com>
2019-10-21 19:43:42 -0400

    añadiendo el component price

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,27 @@
 <template>
   <div id="app">
-    <h1>  My Shop</h1>
-    <p class="animated fadeInRight"> Vue shop test of animation </p>
-    <font-awesome-icon icon="shopping-cart"> </font-awesome-icon>
+    <h1>My Shop</h1>
+    <p class="animated fadeInRight">Vue shop test of animation</p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+    <price value="4.52"></price>
   </div>
 </template>
 
 <script>
-
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Price from "./components/Price.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     FontAwesomeIcon,
+    Price
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Price.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>{{this.prefix + Number.parseFloat(this.value).toFixed(this.precision)}}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Number,
+    prefix: {
+      type: String,
+      default: "$"
+    },
+    precision: {
+      type: Number,
+      default: 2
+    }
+  }
+};
+</script>
+
+   
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
